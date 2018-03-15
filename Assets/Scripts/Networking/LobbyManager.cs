f9331d63cb40d06b40e98b659984e16efa836631
Fernand <fernand.veyrier@epitech.eu>
2018-03-15 07:56:59 +0100

    Added default scenes and demo for networking
    - The demo will eb removed as soon as the real connection works

---
+++ Assets/Scripts/Networking/LobbyManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts.Networking
+{
+    public class LobbyManager : NetworkLobbyManager
+    {
+        private void OnPlayerConnected(NetworkPlayer player)
+        {
+            Debug.Log("Player connected ! " + player.ipAddress);
+        }
+
+        public override void OnLobbyClientConnect(NetworkConnection conn)
+        {
+            base.OnLobbyClientConnect(conn);
+            Debug.Log("Player CONNECTED " + conn.address);
+        }
+    }
+}
---
+++ Assets/Scripts/Networking/LobbyMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Networking
+{
+    public class LobbyMenu : MonoBehaviour
+    {
+        [SerializeField]
+        protected LobbyManager _manager;
+
+        [SerializeField]
+        private Text _textPlayerName;
+
+        public void OnCreateMatchmaking()
+        {
+            _manager.StartMatchMaker();
+            _manager.matchMaker.CreateMatch(_textPlayerName.text, 8, true, "", "", "", 0, 0, _manager.OnMatchCreate);
+        }
+
+        public void OnClickDisplayServerList()
+        {
+            _manager.StartMatchMaker();
+        }
+    }
+}

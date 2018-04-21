13758b9462677a0848c7df0d83f1a3b817370b79
Fernand <fernand.veyrier@epitech.eu>
2018-04-21 15:49:26 +0200

    Added singleton for game manager and trigger for swatter

--- Assets/Scripts/CantRoachThis/SwatterCollider.cs
+++ Assets/Scripts/CantRoachThis/SwatterCollider.cs
@@ -2,24 +2,20 @@
 
 namespace Assets.Scripts.CantRoachThis
 {
+    public delegate void EventTrigger(object caller, Collider other);
+
+    /// <summary>
+    /// Handles collisions with the swatter.
+    /// </summary>
     public class SwatterCollider : MonoBehaviour
     {
+        public event EventTrigger OnTriggerEntered;
+
         private void OnTriggerEnter(Collider other)
         {
             Debug.Log("On trigger enter");
-            if (other.tag.Equals("Player"))
-            {
-                Destroy(other.gameObject);
-            }
-        }
-
-        private void OnCollisionEnter(Collision collision)
-        {
-            Debug.Log("On collision enter");
-            if (collision.transform.tag.Equals("Player"))
-            {
-                Destroy(collision.gameObject);
-            }
+            if (OnTriggerEntered != null)
+                OnTriggerEntered.Invoke(this, other);
         }
     }
 }

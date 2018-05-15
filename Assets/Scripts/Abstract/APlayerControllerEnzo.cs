a71dccfdeab706de3480cdd5a5f57c8995012cd2
gandoulf <enzo.resse@epitech.eu>
2018-05-15 13:37:43 +0200

    base of FillItUp

---
+++ Assets/Scripts/Abstract/APlayerControllerEnzo.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Game;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public abstract class APlayerControllerEnzo : APlayerController
+{
+
+    protected bool looserDrunk = false;
+    public bool LooserDrunk { get { return looserDrunk; } }
+
+    [Command]
+    public void CmdLooserDrunk()
+    {
+        looserDrunk = true;
+    }
+}

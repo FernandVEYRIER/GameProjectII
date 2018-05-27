4e0833a8bee824c6ef4a703c1981e42a1dd65007
Fernand <fernand.veyrier@epitech.eu>
2018-05-27 08:38:19 +0200

    Added controller for nail game

---
+++ Assets/Scripts/IFixIt/NailController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.IFixIt
+{
+    public class NailController : MonoBehaviour
+    {
+        public int HitsNeeded;
+        private int _hitRemaining;
+
+        [SerializeField] private RectTransform _nailRect;
+        [SerializeField] private RectTransform _target;
+        private Vector3 _initialPos;
+        private float _step;
+
+        private void Awake()
+        {
+            _initialPos = _nailRect.position;
+            _step = _initialPos.y - _target.position.y;
+        }
+
+        private void OnEnable()
+        {
+            _hitRemaining = HitsNeeded;
+            _nailRect.position = _initialPos;
+        }
+
+        public void OnPointerDown(BaseEventData data)
+        {
+            _hitRemaining--;
+            var y = _target.position.y + _step * (_hitRemaining / (float)HitsNeeded);
+            Debug.Log(y);
+            _nailRect.position = new Vector3(_nailRect.position.x, y, 1);
+
+            if (_hitRemaining == 0)
+            {
+                Debug.Log("Nail game ended");
+            }
+        }
+    }
+}

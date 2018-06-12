d30fb13ff7388b01d6d072cd445bf85cc633362e
Fernand <fernand.veyrier@epitech.eu>
2018-06-12 08:24:01 +0200

    Optimized shaders

--- Assets/Scripts/UI/WheelSpinner.cs
+++ Assets/Scripts/UI/WheelSpinner.cs
@@ -44,7 +44,7 @@ namespace Assets.Scripts.UI
             {
 #if !RELEASE
                 //LobbyManager.Instance.ChangeScene("Go Soju Go Fast");
-                //Networking.LobbyManager.Instance.ChangeScene("Cant Roach This");
+                Networking.LobbyManager.Instance.ChangeScene("Cant Roach This");
                 //Networking.LobbyManager.Instance.ChangeScene("Darts");
                 //Networking.LobbyManager.Instance.ChangeScene("I Fix It");
                 //Networking.LobbyManager.Instance.ChangeScene("The Drinking Tower");
